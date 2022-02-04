3d3c24c2ac6cc21fcebd5431fe2736e85eeb27a3
JL <lipabass@gmail.com>
2022-02-04 15:18:19 +0100

    Guessing Game v2.1, README.md update

--- README.md
+++ README.md
@@ -3,22 +3,26 @@
 
 This repository contains simple, training projects I have created during courses and bootcamps.
 
-## Table of content
-* Python 3.9.7
+## Content
+### Python 3.9.7
  * Lotto Generator
 
    My very first project in python. Generates desired number of 6 unique, random number sets, and saves them in "wyniki lotto.txt".
 
  * Guessing Game
 
-   First assigment from the Perian Data 2022 Complete Python Bootcamp. Player has to guess the number between 1 and 100.
+   First assignment from the Perian Data 2022 Complete Python Bootcamp. Player has to guess the number between 1 and 100.
    The game tells, whether each guess is closer to the target than the previous one. It also tells whether the first guess
    is closer, or further than 10 from the target.
 
  * Guessing Game 2
 
    Added some improvements to the original Guessing Game:
-   * a plot showing player's path to guessing the mystery number
-
+   * v2.0:
+     * a plot showing player's path to guessing the mystery number.
+   * v2.1:
+     * ignores the guess if guess value is the same as the previous one, and prompt "Give me something new" message.
+     * asks for a value again if player puts non integer value as a guess or presses ENTER key without entering a number.
+     *returns "WARM!" or "COLD!" if the distance between new guess and the previous one is the same. "WARM!" if the distance is smaller than two guesses later, or than the initial cold/warm threshold, "COLD!" otherwise.
 
 *JL 2022*
--- python/GuessingGameChallenge/GuessingGameChallenge2.py
+++ python/GuessingGameChallenge/GuessingGameChallenge2.py
@@ -1,46 +1,61 @@
 #Guessing Game by JL (lipabass@gmail.com)
 #Challenge from the Complete Python 3 Bootcamp by Pierian Data
+#v2.1
 
 from random import randint
 
-x = randint(1,100)
-delta = 10
-guess = None
-guesses = []
-plot_line = ''
+x = randint(1,100)		#The Mystery Number
+delta = [10]			#list of differences between subsequent guesses and the mystery number. [0] is the initial warm/cold threshold
+guess = None			#current guessed value
+guesses = []			#list of players guesses
+plot_line = ''			#chart line
 
 print('\nWelcome to the Guessing Game!\n')
 
 while guess != x:
-	guess = int(input('Guess a number between 1 and 100:\n'))
-	
-	if guess < 1 or guess > 100:
-		print(f'OUT OF BOUNDS!')
-		continue	
+	guess = input('Guess a number between 1 and 100:\n')
 
-	guesses.append(guess)	
+	if guess == '' or guess.isdigit() != True:				#checks if guess is a digit
+		continue
 
-	delta_new = abs(x - guess)
-	if len(guesses) > 1:
-		if delta_new < delta:
-			print('WARMER!\n')
-		else:
-			print('COLDER!\n')	
-	else:
-		if delta_new < delta:
-			print('WARM!\n')
-		else:
-			print('COLD!\n')
-		
-	delta = delta_new
+	guess = int(guess)
+
+	if guess < 1 or guess > 100:							#checks if guess is within a given range
+		print('OUT OF BOUNDS!')
+		continue
+
+	guesses.append(guess)
+
+	delta.append(abs(x - guess))
+	if len(guesses) > 1: 									#any guess other than 1st
+		if guesses[-1] == guesses[-2]:						#exactly same value
+			print('Give me something new!\n')
+			guesses.pop(-1)
+			continue
+		else:												#new value
+			print('\n')
+			if delta[-1] < delta[-2]:
+				print('WARMER!\n')
+			elif delta[-1] == delta[-2] and delta[-1] <= delta[-3]:
+				print('WARM!\n')
+			elif delta[-1] == delta[-2] and delta[-1] > delta[-3]:
+				print('COLD!\n')
+			else:
+				print('COLDER!\n')
+	else:													#the 1st guess
+			if delta[-1] < delta[-2]:
+				print('WARM!\n')
+			else:
+				print('COLD!\n')
+
 
 print('CONGRATULATIONS!\nThe mystery number is {}, it took you {} turns to find it!\n'.format(x, len(guesses)))
 
-for y in range(len(guesses)):
+for y in range(len(guesses)):								#chart plot
 	plot_line += '{0:4}'.format(str(y+1)+'.') + '|'
 	for j in range(100):
 		if j+1 == x == guesses[y]:
-			plot_line += 'X'	
+			plot_line += 'X'
 		elif j+1 == x:
 			plot_line += '.'
 		elif j+1 == guesses[y]:
@@ -53,4 +68,3 @@ for y in range(len(guesses)):
 
 
 print('\nGuessing Game by JL for the Pierian Data Guessing Game Challenge\n2022')
-
